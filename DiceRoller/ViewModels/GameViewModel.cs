a12d78d0a9b3aaf39484646657089e60fcf76980
Julia Reis <julyreis.correa@gmail.com>
2025-05-30 22:30:28 -0300

    Interfaces, Models, etc (part 2)

--- DiceRoller/ViewModels/GameViewModel.cs
+++ DiceRoller/ViewModels/GameViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DiceRoller.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace DiceRoller.ViewModels
 {
@@ -15,9 +17,34 @@ namespace DiceRoller.ViewModels
         [ObservableProperty]
         private string nomeUsuario;
 
+        [ObservableProperty]
+        private int selectedValue;
+
+        [ObservableProperty]
+        private string resultado;
+
         public GameViewModel(){
-            diceImage = "";
+            diceImage = "dado1.png";
             nomeUsuario = "Eu";
+            JogarCommand = new Command(Jogar);
+        }
+
+        private ICommand JogarCommand { get; private set; }
+
+        public void Jogar()
+        {
+            //sortear dado
+            Dice dice = new Dice();
+            dice.Roll();
+            if (dice.FaceParaCima == SelectedValue){
+                Resultado = "Vitória";
+            }
+            else {
+                Resultado = "Derrota";
+            }
+            //verificar a face
+            //verificar se escolheu certo
+            //exibir uma imagem na tela (exemplo)
         }
     }
 }
